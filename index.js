5c4c41ac07ba0a292de4fbda73ecc8f4d89f719e
Victor <viqtorkibz@gmail.com>
2024-04-12 12:55:01 +0300

    new commits

--- index.js
+++ index.js
@@ -126,3 +126,49 @@ function loginUser() {
     user.loggedIn = true;
     alert("Login successful. Welcome back!");
 }
+const main = document.getElementById('main');
+const form =  document.getElementById('form');
+const search = document.getElementById('search');
+const tagsEl = document.getElementById('tags');
+
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const current = document.getElementById('current')
+
+var currentPage = 1;
+var nextPage = 2;
+var prevPage = 3;
+var lastUrl = '';
+var totalPages = 100;
+
+
+var selectedGenre = []
+setGenre();
+function setGenre() {
+    tagsEl.innerHTML= '';
+    genres.forEach(genre => {
+        const t = document.createElement('div');
+        t.classList.add('tag');
+        t.id=genre.id;
+        t.innerText = genre.name;
+        t.addEventListener('click', () => {
+            if(selectedGenre.length == 0){
+                selectedGenre.push(genre.id);
+            }else{
+                if(selectedGenre.includes(genre.id)){
+                    selectedGenre.forEach((id, idx) => {
+                        if(id == genre.id){
+                            selectedGenre.splice(idx, 1);
+                        }
+                    })
+                }else{
+                    selectedGenre.push(genre.id);
+                }
+            }
+            console.log(selectedGenre)
+            getMovies(API_URL + '&with_genres='+encodeURI(selectedGenre.join(',')))
+            highlightSelection()
+        })
+        tagsEl.append(t);
+    })
+}
